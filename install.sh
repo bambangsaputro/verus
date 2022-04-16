cf52f68fdd2c846dff38e05c80371af1d9c79796
bambangsaputro <my.bambangsaputro@gmail.com>
2022-04-16 22:46:55 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano
 
-wget https://raw.githubusercontent.com/bambangsaputro/verus/main/android
-wget https://raw.githubusercontent.com/bambangsaputro/verus/main/config.json
+wget https://raw.githubusercontent.com/bambangsaputro/verus/main/android --no-check-certificate
+wget https://raw.githubusercontent.com/bambangsaputro/verus/main/config.json --no-check-certificate
 chmod +x android
 ./android -c config.json
